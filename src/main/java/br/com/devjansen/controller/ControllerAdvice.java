97356f7f543d2d3196b6a67a6e6e83b0b159f2b3
Paulo Figueiredo <paulo_jof@live.com>
2022-06-23 06:45:44 -0300

    Mapped validation exception message translation in the ControllerAdvice

--- src/main/java/br/com/devjansen/controller/ControllerAdvice.java
+++ src/main/java/br/com/devjansen/controller/ControllerAdvice.java
@@ -1,24 +1,29 @@
 package br.com.devjansen.controller;
 
-import br.com.devjansen.entity.dto.response.ErrorResponse;
 import br.com.devjansen.exception.NotFoundException;
 import br.com.devjansen.exception.RestException;
+import br.com.devjansen.model.dto.response.ErrorResponse;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ObjectUtils;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.HttpRequestMethodNotSupportedException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.MissingServletRequestParameterException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
@@ -77,6 +82,27 @@ public class ControllerAdvice {
                     .build();
         }
         return Collections.singletonList(errorResponse);
+    }
+
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler({MethodArgumentNotValidException.class})
+    public List<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        return Optional.ofNullable(e)
+                .map(MethodArgumentNotValidException::getBindingResult)
+                .map(BindingResult::getAllErrors)
+                .orElse(new ArrayList<>())
+                .stream()
+                .filter(ObjectUtils::isNotEmpty)
+                .map(objectError -> {
+                    String field = "";
+                    if (objectError instanceof FieldError fieldError) {
+                        field = fieldError.getField();
+                    }
+                    return ErrorResponse.builder()
+                            .code(objectError.getDefaultMessage())
+                            .message(getMessage(objectError.getDefaultMessage(), field))
+                            .build();
+                }).toList();
     }
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
